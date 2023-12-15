ae02c7503ed7f4109f5015b95b0596a9de8e93a3
Joel Heaps <joel@joelheaps.com>
2023-12-14 22:02:28 -0600

    Add tests for day14

--- src/day14/test_solve.py
+++ src/day14/test_solve.py
@@ -1,4 +1,9 @@
-from .solve import shift_rocks_in_line
+from .solve import (
+    shift_rocks_in_line,
+    slide_rocks_north,
+    slide_rocks_west,
+    slide_rocks_south,
+)
 
 
 def test_shift_rocks_in_line():
@@ -10,3 +15,40 @@ def test_shift_rocks_in_line():
 
     line = "#..O..O"
     assert shift_rocks_in_line(line) == "#OO...."
+
+
+def test_slide_rocks_north():
+    pattern = """..
+..
+OO"""
+
+    assert (
+        slide_rocks_north(pattern)
+        == """OO
+..
+.."""
+    )
+
+
+def test_slide_rocks_west():
+    pattern = """.O.
+#.O"""
+
+    assert (
+        slide_rocks_west(pattern)
+        == """O..
+#O."""
+    )
+
+
+def test_slide_rocks_south():
+    pattern = """OO
+..
+#."""
+
+    assert (
+        slide_rocks_south(pattern)
+        == """..
+O.
+#O"""
+    )
